fab8f8e76b4296695509c2ad0cfcd86cfb63fa45
Hieu Nguyen <hieumum@gmail.com>
2018-12-26 23:35:19 -0800

    check-in 2018-12-27

---
+++ projs/c3/Objects.js
@@ -0,0 +1,20 @@
+var o = new Object();
+console.log(o); //{} empty object
+var now = new Date();
+console.log(now);
+
+var arr = new Array();
+console.log(arr); //[] empty array
+ 
+var pattern = new RegExp("\\sjava\\s", "i");
+
+var point = new Object();
+point.x = 2.3;
+point.y = -1.2;
+console.log(point);
+
+var point2 = {
+    x: 2.3,
+    y:-1.2
+};
+console.log(point2);
