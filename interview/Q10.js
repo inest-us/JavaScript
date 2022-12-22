4508fe10dbaae42c3cc894c6f797aa721e87f590
inest-us <hieung@live.com>
2022-12-21 22:27:19 -0600

    JS interview questions 2

---
+++ interview/Q10.js
@@ -0,0 +1,5 @@
+let goodbye = async () => {
+    return "Goodbye";
+}
+
+goodbye().then(console.log);
---
+++ interview/Q13.js
@@ -0,0 +1,8 @@
+let x = 10;
+
+function myFunc(y) {
+    y = 12;
+}
+
+myFunc(x);
+console.log(x);
